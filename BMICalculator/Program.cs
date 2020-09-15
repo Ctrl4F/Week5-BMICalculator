6d34495e0747251a022b23882f68c6142bb84ccf
Opilane TTHK <opilane@tthk.ee>
2020-09-15 12:11:41 +0300

    BMICalculator

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = Int32.Parse(Console.ReadLine());
+            
+            
+            if(age <= 28)
+            {
+                Console.WriteLine("You are too young to use the calculator");
+            }else if(age >= 60)
+            {
+                Console.WriteLine("you are too old to use the calculator");
+            }
+            else
+            {
+                double height;
+                Console.WriteLine("Enter your height:");
+                height = double.Parse(Console.ReadLine());
+                Console.WriteLine($"your height is {height} meters.");
+
+            }
+            
+            
+
+            
+
+
+        }
+    }
+}
